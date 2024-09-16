2e4b17f9ba818b36e1d72d8ca67965bdeba204fb
Adam Jasinski <adam@jasinski.lt>
2024-09-16 20:11:48 +0300

    initial commit

---
+++ config/settings.nix
@@ -0,0 +1,54 @@
+{
+  config = {
+    extraConfigLuaPre =
+      # lua
+      ''
+        vim.fn.sign_define("diagnosticsignerror", { text = " ", texthl = "diagnosticerror", linehl = "", numhl = "" })
+        vim.fn.sign_define("diagnosticsignwarn", { text = " ", texthl = "diagnosticwarn", linehl = "", numhl = "" })
+        vim.fn.sign_define("diagnosticsignhint", { text = "󰌵", texthl = "diagnostichint", linehl = "", numhl = "" })
+        vim.fn.sign_define("diagnosticsigninfo", { text = " ", texthl = "diagnosticinfo", linehl = "", numhl = "" })
+      '';
+
+    clipboard = {
+      providers.xclip.enable = true;
+    };
+
+    opts = {
+      number = true;
+
+      mouse = "a";
+
+      showmode = false;
+
+      clipboard = "unnamedplus";
+
+      breakindent = true;
+
+      undofile = true;
+
+      cursorline = true;
+
+      splitright = true;
+
+      splitbelow = true;
+
+      updatetime = 250;
+
+      timeoutlen = 300;
+      
+      signcolumn = "yes";
+
+      list = true;
+
+      listchars = { tab = "» "; trail = "·"; nbsp = "␣"; };
+
+      inccommand = "split";
+
+      scrolloff = 10;
+
+      hlsearch = true;
+    };
+
+    colorschemes.catppuccin.enable = true;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,63 @@
+# Inspired by https://github.com/dc-tec/nixvim
+{
+  description = "adezxc NeoVim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    nixvim,
+    flake-parts,
+    pre-commit-hooks,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {
+        system,
+        pkgs,
+        self',
+        lib,
+        ...
+      }: let
+        nixvim' = nixvim.legacyPackages.${system};
+        nvim = nixvim'.makeNixvimWithModule {
+          inherit pkgs;
+          module = ./config;
+        };
+      in {
+        checks = {
+          default = pkgs.nixvimLib.check.mkTestDerivationFromNvim {
+            inherit nvim;
+            name = "A nixvim configuration";
+          };
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              statix.enable = true;
+              alejandra.enable = true;
+            };
+          };
+        };
+
+        formatter = pkgs.alejandra;
+
+        packages.default = nvim;
+
+        devShells = {
+          default = with pkgs;
+            mkShell {inherit (self'.checks.pre-commit-check) shellHook;};
+        };
+      };
+    };
+}
